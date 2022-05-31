a016d5cdd49956bee949257e01ae21f6185d63d4
Elena de Lara <elena_delara@hotmail.com>
2022-05-31 13:09:59 +0200

    dash

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -3,4 +3,49 @@ class ProjectsController < ApplicationController
     @projects = policy_scope(Project)
   end
 
+  def new
+    @project = Project.new
+    authorize @project
+  end
+
+  def create
+    @project = Project.new(project_params)
+    @project.user = current_user
+    authorize @project
+    if @project.save
+      redirect_to project_path(@project)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @project = Project.find(params[:id])
+  end
+
+  def update
+    @project.update(project_params)
+    redirect_to project_path(@project)
+  end
+
+  def destroy
+    @project = Project.find(params[:id])
+    authorize @project
+    @project.destroy
+    redirect_to dashboard_path, status: :see_other
+  end
+
+  private
+
+  def project_params
+    params.require(:project).permit(
+      :name,
+      :start_at,
+      :end_at,
+      :progress,
+      :company_name,
+      :company_email
+    )
+  end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -19,3 +19,25 @@ user_one = User.new(
 
 user_one.law_firm = law_firm
 user_one.save!
+
+user_two = User.new(
+  email: "toto@flipeasy.fr",
+  first_name: "toto",
+  last_name: "Thing",
+  password: "123459"
+)
+
+user_two.law_firm = law_firm
+user_two.save!
+
+
+project = Project.new(
+  name: "XX",
+  start_at: "10/20/2022",
+  end_at: "10/20/2022",
+  progress: "20%",
+  company_name: "blabla",
+  company_email: "hh@flipeasy.fr"
+  )
+project.user = user_two
+project.save!
