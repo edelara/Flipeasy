9d5248f1bf505ee002a8e197de9ecf6e21e03645
rena kakon <renakakon@gmail.com>
2022-05-31 16:03:21 +0200

    completed tasks and steps

--- app/models/project.rb
+++ app/models/project.rb
@@ -4,12 +4,20 @@ class Project < ApplicationRecord
   belongs_to :user
 
   validates :name, presence: true, uniqueness: true
+  validates :start_at, presence: true
   validates :company_name, presence: true
   validates :company_email, presence: true
 
-  # after_create :create_steps_and_tasks
+  after_validation :set_end_at
+  after_create :create_steps_and_tasks
 
   private
+
+  def set_end_at
+    return unless end_at.blank?
+
+    self.end_at = start_at + 60.days
+  end
 
   def create_steps_and_tasks
     CreateStepsAndTasksForProject.new(self).call
--- app/services/create_steps_and_tasks_for_project.rb
+++ app/services/create_steps_and_tasks_for_project.rb
@@ -2,18 +2,56 @@ class CreateStepsAndTasksForProject
 
   STEPS_AND_TASKS = {
     "Create Project" => {
-      deadline: 30,
+      deadline: 29,
       tasks: [
-        "Invite founder to join Flipeasy (to complete questionnaire and upload legal documents of the company)",
-        "Review questionnaire"
+        "Invite founder to join Flipeasy",
+        "Review questionnaire",
+        "Create a team and assign steps",
+        "Schedule kick-off call with the founder"
       ]
     },
     "Getting Ready" => {
-      deadline: 25,
+      deadline: 27,
       tasks: [
         "Draft flip Pro-forma model",
-        "Determine the valuation of the company"
+        "Determine the valuation of the company",
+        "Consult tax team"
       ]
+
+    "Establish the U.S Corporation" => {
+      deadline: 20,
+      tasks: [
+        "Draft Incorporation Documents",
+        "File Certificate of Incorporation"
+        ]
+    }
+
+    "Obtain EIN and open U.S. Bank Account" => {
+      deadline: 15,
+    }
+
+    "Issuance & Exchange of Shares" => {
+      deadline: 10,
+      tasks: [
+        "Draft Flip Documents"
+        ]
+    }
+
+    "Flip Closing" => {
+      deadline: 05,
+      tasks: [
+        "Invite signatories to sign"
+        ]
+    }
+
+    "Post-Flip Closing" => {
+      deadline: 03,
+      tasks: [
+        "File 83(b) election"
+        "File Contribution Agreement"
+        "Update Shareholders Registers & Accounts"
+        "Schedule Reminder for Delaware Tax Compliance"
+        ]
     }
   }
 
