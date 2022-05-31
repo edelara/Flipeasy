27524f1e0690e40431813583c71b9905b118fa2a
Lydia Soula soulalydia1@gmail.com <soulalydia1@gmail.com>
2022-05-31 12:26:04 +0200

    validations-models

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,4 +2,8 @@ class Project < ApplicationRecord
   has_many :documents
   has_many :steps
   belongs_to :user
+
+  validates :name, presence: true
+  validates :company_name, presence: true
+  validates :company_email, presence: true
 end
