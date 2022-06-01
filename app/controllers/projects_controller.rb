6539f2976cbbd452c8fb7efcdc3497192c72995c
Elena de Lara <elena_delara@hotmail.com>
2022-06-01 15:49:14 +0200

    show fixed

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -12,6 +12,11 @@ class ProjectsController < ApplicationController
     authorize @project
   end
 
+  def show
+    @project = Project.find(params[:id])
+    authorize @project
+  end
+
   def create
     @project = Project.new(project_params)
     @project.user = current_user
