c8cbf895fdaaddd6d3d53a9ca1dfc712045c8d4e
Elena de Lara <elena_delara@hotmail.com>
2022-06-01 10:23:41 +0200

    changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema[7.0].define(version: 2022_05_31_105758) do
-=======
 ActiveRecord::Schema[7.0].define(version: 2022_05_31_151608) do
->>>>>>> 64918e039b62aaad0b09bacead37d48635c9afa0
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
