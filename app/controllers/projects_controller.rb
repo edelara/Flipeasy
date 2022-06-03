d95c5287f3cf4afb157f287d391deb9d366004e5
Camille Riviere <camille.riviere6@gmail.com>
2022-06-03 15:22:03 +0200

    new form and index width

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -24,7 +24,7 @@ class ProjectsController < ApplicationController
     @project.user = current_user
     authorize @project
     if @project.save
-      redirect_to project_path(@project)
+      redirect_to project_steps_path(@project)
     else
       render :new, status: :unprocessable_entity
     end
