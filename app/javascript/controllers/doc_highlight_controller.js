d57b90bc244ad9021bcfbe018097a58a4f8f21f2
rena kakon <renakakon@gmail.com>
2022-06-06 17:13:24 +0200

    show document for each tasks and flatpickr

---
+++ app/javascript/controllers/doc_highlight_controller.js
@@ -0,0 +1,22 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="doc-highlight"
+export default class extends Controller {
+  static targets = ["doc"]
+  connect() {
+    console.log(this.docTargets);
+  }
+  setActive(event) {
+    const documents = JSON.parse(event.currentTarget.dataset.documents);
+    this.docTargets.forEach(docTarget => docTarget.classList.add('d-none'));
+    documents.forEach(doc => {
+      const d = this.docTargets.find(docTarget => docTarget.dataset.docName == doc);
+      if (d) d.classList.remove("d-none");
+    })
+  }
+
+  showAll(event) {
+    event.preventDefault()
+    this.docTargets.forEach(docTarget => docTarget.classList.remove('d-none'));
+  }
+}
