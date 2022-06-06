4a544d072ab2150a4a72299289d7222961fcace1
Camille Riviere <camille.riviere6@gmail.com>
2022-06-06 11:46:37 +0200

    commit to get the panel

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,16 @@
 class TasksController < ApplicationController
+  def update
+    @task = Task.find(params[:id])
+    authorize @task
+    @task.user_id = params[:user_id]
+    @task.save
+    render json: @task.to_json
+  end
+
+  def completed
+    ###
+    @task.update
+    @step = Step.find(params[:task_id])
+    @step.completed if @task == @step.tasks.last
+  end
 end
