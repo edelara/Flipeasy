4044aa64cb676ce1850701a745a4090d8b650452
rena kakon <renakakon@gmail.com>
2022-06-07 17:48:00 +0200

    seeded and fixed

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -10,9 +10,6 @@ application.register("doc-highlight", DocHighlightController)
 import DocumentController from "./document_controller.js"
 application.register("document", DocumentController)
 
-import DocHighlightController from "./doc_highlight_controller.js"
-application.register("doc-highlight", DocHighlightController)
-
 import DragController from "./drag_controller.js"
 application.register("drag", DragController)
 
